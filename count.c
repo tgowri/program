6500eb884a8bceafcd43d28cf6efa888d66f5714
tgowri <gowrithulasiraj@gmail.com>
2019-02-01 11:57:54 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i=0,n=0;
+char string[100],a;
+clrscr();
+gets(string);
+while(str[i]!='\0')
+{
+a=str[i];
+if(a>='A' && a<='Z' || a>='a' && a<='z')
+n++;
+}
+prinf("%d",n);
+}

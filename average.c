c98f244ab056e82af7bc57e1d8cfd9b0aeb27791
tgowri <gowrithulasiraj@gmail.com>
2019-02-20 13:54:55 +0530

    Create average.c

---
+++ average.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int limit,i,number[100],sum=0,average;
+scanf("%d",&limit);
+for(i=0;i<limit;i++)
+{
+scanf("%d",&number[i];
+}
+for(i=0;i<count;i++)
+{
+sum=sum+number[i];
+}
+average=sum/limit;
+printf("%d",average);
+return 0;
+}
